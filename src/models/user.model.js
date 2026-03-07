65fce3750aebeb49ed9021aac6fbd7c68b2f7565
Kalpit Agrawal <kalpitagrawal06@gmail.com>
2026-03-07 23:58:13 +0530

    fix all bugs regarding register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -28,7 +28,7 @@ const userSchema = new Schema({
         type: String, //cloudinary url
         required: true,
     },
-    coverimage: {
+    coverImage: {
         type: String,
     },
     watchHistory: [
@@ -48,11 +48,11 @@ const userSchema = new Schema({
     timestamps: true
 })
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
     if (!this.isModified("password")) return next();
 
     this.password = await bcrypt.hash(this.password, 10);
-    next();
+
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
