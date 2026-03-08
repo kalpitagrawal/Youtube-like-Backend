e116edd8a984126f296f5e3cb40740228826b881
Kalpit Agrawal <kalpitagrawal06@gmail.com>
2026-03-09 02:53:20 +0530

    remove next related bug from User model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-
+import { ref } from "process";
 
 const generateAccessAndRefreshTokens = async (userId) => {
     try {
