81d874f1f2d9fc71a3c990568407847464cd0ede
Kalpit Agrawal <kalpitagrawal06@gmail.com>
2026-03-11 00:54:21 +0530

    feat: add healthcheck route and controller for server status monitoring

--- src/app.js
+++ src/app.js
@@ -41,11 +41,13 @@ Middleware sends the response to client */
 
 //routes import 
 
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 import userRouter from "./routes/user.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 
 // routes declaration
-app.use("/api/v1/users", userRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tweets", tweetRouter);
 
 export { app };
